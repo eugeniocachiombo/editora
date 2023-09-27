889c1fb2d4d06cadaf2b681afe5d990f50557071
eugeniocachiombo <eugeniocachiombo@gmail.com>
2023-09-28 00:15:27 +0100

    Erro de cookie novamente resolvido

--- app/core/verificarSessao.php
+++ app/core/verificarSessao.php
@@ -2,22 +2,24 @@
 if ( empty( $_SESSION[ 'idUtilizador' ] ) ) {
     ?>
         <script>
-           Swal.fire({
-                        title: 'Erro de sessão!',
-                        text: "Pretende iniciar sessão?",
-                        icon: 'warning',
-                        showCancelButton: true,
-                        confirmButtonColor: '#3085d6',
-                        cancelButtonColor: '#d33',
-                        confirmButtonText: 'Sim, pretendo',
-                        cancelButtonText: "Cancelar"
-            }).then((result) => {
-                    if (result.isConfirmed) {
-                        window.location = "/login";
-                    }else{
-                        window.location = "/Impossivel visualizar esta página ";
-                    }
-            });
+                    Swal.fire({
+                            title: 'Erro de sessão!',
+                            text: "Pretende iniciar sessão?",
+                            icon: 'warning',
+                            showCancelButton: true,
+                            confirmButtonColor: '#3085d6',
+                            cancelButtonColor: '#d33',
+                            confirmButtonText: 'Sim, pretendo',
+                            cancelButtonText: "Cancelar"
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            localStorage.clear("cookies");
+                            window.location = "/login";
+                        }else{
+                            window.location = "/Impossivel visualizar esta página ";
+                        }
+                    });
+            
         </script>
     <?php
 }
