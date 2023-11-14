74e9952195653763bdcdce48cda567b87fd45800
eugeniocachiombo <eugeniocachiombo@gmail.com>
2023-11-14 17:02:54 +0100

    Barra de rolagem só aparece após se passado o mouse no menu

--- app/views/painel/inclusao/card/card.php
+++ app/views/painel/inclusao/card/card.php
@@ -1,5 +1,5 @@
 <div class="card col-4" style="position: fixed; max-height: 90vh; margin-top: 108px; background: #57489b; width: 300px; ">
-    <div class="card-body" style="overflow-y: scroll; background:#57489b; margin-left: -15px; margin-top: -2px;"  >
+    <div class="card-body" style="background:#57489b; margin-left: -15px; margin-top: -2px;"  >
         <?php require "../app/views/painel/inclusao/card/listaCard/lista.php"; ?>
 
         <?php require "../app/views/painel/inclusao/card/btnTerminarSessao.php"; ?>
