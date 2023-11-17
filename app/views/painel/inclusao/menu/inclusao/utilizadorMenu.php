bb54d60a29c09a58d0c36e08f994ba2fa8bf6d8b
eugeniocachiombo <eugeniocachiombo@gmail.com>
2023-11-17 11:00:33 +0100

    Melhoramento foto de perfil

--- app/views/painel/inclusao/menu/inclusao/utilizadorMenu.php
+++ app/views/painel/inclusao/menu/inclusao/utilizadorMenu.php
@@ -2,7 +2,7 @@
 <div class="col d-none d-lg-flex justify-content-center align-items-center">
     <div class="col text-center ">
         <?php include "../app/views/painel/inclusao/menu/inclusao/fotoPerfil.php"; ?>
-        <span class="text-white"><?php echo $_SESSION["nomeArtisticoUtilizador"]; ?></span>
+        <span class="text-white" style="font-weight: bold"><?php echo $_SESSION["nomeArtisticoUtilizador"]; ?></span>
     </div>
 </div>
 
@@ -10,7 +10,7 @@
 <div class="col d-lg-none justify-content-center align-items-center">
     <div class="col text-center ">
         <?php include "../app/views/painel/inclusao/menu/inclusao/fotoPerfil.php"; ?> <br>
-        <span class="text-white"><?php echo $_SESSION["nomeArtisticoUtilizador"]; ?></span>
+        <span class="text-white" style="font-weight: bold"><?php echo $_SESSION["nomeArtisticoUtilizador"]; ?></span>
         <hr>
     </div>
 </div>
